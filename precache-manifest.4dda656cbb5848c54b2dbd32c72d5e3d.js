51e7a2ae8d986a75faeb0020f154f0092f3bc6ea
Roagen <harek2388@gmail.com>
2019-08-24 21:47:50 +0200

    Updates

---
+++ precache-manifest.4dda656cbb5848c54b2dbd32c72d5e3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7e08c791a09540749263a8a3918020",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "6074aa202e11963f88e9",
+    "url": "/calc/static/css/main.797ca23e.chunk.css"
+  },
+  {
+    "revision": "0664472df9a71effd89a",
+    "url": "/calc/static/js/2.147fb21e.chunk.js"
+  },
+  {
+    "revision": "6074aa202e11963f88e9",
+    "url": "/calc/static/js/main.25e03f98.chunk.js"
+  },
+  {
+    "revision": "8977a1fd750b8ebd47fe",
+    "url": "/calc/static/js/runtime~main.f6aef684.js"
+  }
+]);
